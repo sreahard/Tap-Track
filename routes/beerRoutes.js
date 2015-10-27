5133e0c1a229baf1320c6c6897d0477482660e84
Trevor Finsaas <finsaasfamily@hotmail.com>
2015-10-27 11:46:29 -0600

    Working server, postman works for all functions

---
+++ routes/beerRoutes.js
@@ -0,0 +1,111 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var Beer = require('../model/beerModel');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.route('/')
+	.get(function(req, res){
+		mongoose.model('Beer').find({}, function(err, beer){
+			if(err){
+				return console.log('err');
+			} else {
+				res.json(beer);
+			}
+		});
+	})
+
+	.post(function(req, res){
+		var name =  req.body.name;
+		var category = req.body.category;
+		var ibu = req.body.ibu;
+		var abv = req.body.abv;
+		var brewery = req.body.brewery;
+		var description = req.body.description;
+		mongoose.model('Beer').create({
+			name: name,
+			category: category,
+			ibu: ibu,
+			abv: abv,
+			brewery: brewery,
+			description: description
+
+		},
+		function(err, beer){
+			if(err){
+				res.send("not working");
+			} else {
+				console.log("New beer named " + beer + " created!");
+				res.send(beer);
+			}
+		});
+	});
+
+router.route('/:id')
+	.get(function(req, res) {
+		mongoose.model('Beer').findById({
+			_id: req.params.id
+		}, function(err, beer) {
+			if(err)
+				res.send(err);
+				res.json(beer);
+		});
+	})
+
+	.put(function(req, res) {
+		mongoose.model('Beer').findById(req.params.id, function(err, beer){
+			if(err)
+				res.send(err);
+			beer.name = req.body.name;
+			beer.category = req.body.category;
+			beer.ibu = req.body.ibu;
+			beer.abv = req.body.abv;
+			beer.brewery = req.body.brewery;
+			beer.description = req.body.description;
+			console.log(JSON.stringify(beer));
+
+			beer.save(function(err) {
+				if(err)
+					res.send(err);
+					res.json({ message: "Beer was updated"});
+			});
+
+		});
+	})
+
+	.delete(function(req, res) {
+		mongoose.model('Beer').remove({
+			_id: req.params.id
+		}, function(err, beer) {
+			if(err)
+				res.send(err);
+				res.json({ message: 'Successfully Deleted'});
+		});
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+var db = require('./model/db');
+
+
+
+var beerModel = require ('./model/beerModel');
+var beerRoutes = require ('./routes/beerRoutes');
+
+//Routes========================================================================
+app.use('/api/beer', beerRoutes);
+
+
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 // use res.render to load up an ejs view file
 
@@ -14,6 +29,10 @@ app.get('/', function(req, res) {
 // about page 
 app.get('/about', function(req, res) {
     res.render('pages/about');
+});
+
+app.get('/enter_beer', function(req, res) {
+    res.render('pages/enter_beer');
 });
 
 app.listen(9090);
