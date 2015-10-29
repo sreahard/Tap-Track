b7c5340bd77bc565c25fade3a3b78db30045b124
Trevor Finsaas <finsaasfamily@hotmail.com>
2015-10-29 14:14:50 -0600

    Linked twitter/facebook/local accounts and unlink options

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -72,19 +72,6 @@ module.exports = function(app, passport) {
         res.redirect('/');
     });
 
-
-
-    // route middleware to make sure a user is logged in
-    // function isLoggedIn(req, res, next) {
-
-    //     // if user is authenticated in the session, carry on
-    //     if (req.isAuthenticated())
-    //         return next();
-
-    //     // if they aren't redirect them to the home page
-    //     res.redirect('/');
-    // }
-
     // =====================================
     // TWITTER ROUTES ======================
     // =====================================
@@ -98,6 +85,68 @@ module.exports = function(app, passport) {
             failureRedirect : '/'
         }));
 
+    // locally --------------------------------
+        app.get('/connect/local', function(req, res) {
+            res.render('connect-local.ejs', { message: req.flash('loginMessage') });
+        });
+        app.post('/connect/local', passport.authenticate('local-signup', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        }));
+
+    // facebook -------------------------------
+
+        // send to facebook to do the authentication
+        app.get('/connect/facebook', passport.authorize('facebook', { scope : 'email' }));
+
+        // handle the callback after facebook has authorized the user
+        app.get('/connect/facebook/callback',
+            passport.authorize('facebook', {
+                successRedirect : '/profile',
+                failureRedirect : '/'
+            }));
+
+    // twitter --------------------------------
+
+        // send to twitter to do the authentication
+        app.get('/connect/twitter', passport.authorize('twitter', { scope : 'email' }));
+
+        // handle the callback after twitter has authorized the user
+        app.get('/connect/twitter/callback',
+            passport.authorize('twitter', {
+                successRedirect : '/profile',
+                failureRedirect : '/'
+            }));
+
+        // local -----------------------------------
+    app.get('/unlink/local', function(req, res) {
+        var user            = req.user;
+        user.local.email    = undefined;
+        user.local.password = undefined;
+        user.save(function(err) {
+            res.redirect('/profile');
+        });
+    });
+
+    // facebook -------------------------------
+    app.get('/unlink/facebook', function(req, res) {
+        var user            = req.user;
+        user.facebook.token = undefined;
+        user.save(function(err) {
+            res.redirect('/profile');
+        });
+    });
+
+    // twitter --------------------------------
+    app.get('/unlink/twitter', function(req, res) {
+        var user           = req.user;
+        user.twitter.token = undefined;
+        user.save(function(err) {
+           res.redirect('/profile');
+        });
+    });
+
     
     //route middleware to make sure a user is logged in
     function isLoggedIn(req, res, next) {
@@ -110,3 +159,6 @@ module.exports = function(app, passport) {
         res.redirect('/');
     };
 };
+
+
+
