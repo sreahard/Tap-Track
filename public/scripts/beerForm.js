cc7f6f492a1971f7bf4746d31a31647e43e666e1
sreahard <suziequz@msn.com>
2015-10-30 11:56:02 -0600

    more styling stuff

--- public/scripts/beerForm.js
+++ public/scripts/beerForm.js
@@ -1,3 +1,46 @@
+var FileForm = React.createClass({
+
+  // since we are starting off without any data, there is no initial value
+  getInitialState: function() {
+    return {
+      data_uri: null,
+    };
+  },
+
+  // prevent form from submitting; we are going to capture the file contents
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+  },
+
+  // when a file is passed to the input field, retrieve the contents as a
+  // base64-encoded data URI and save it to the component's state
+  handleFile: function(e) {
+    var self = this;
+    var reader = new FileReader();
+    var file = e.target.files[0];
+
+    reader.onload = function(upload) {
+      self.setState({
+        data_uri: upload.target.result,
+      });
+    }
+
+    reader.readAsDataURL(file);
+  },
+
+  // return the structure to display and bind the onChange, onSubmit handlers
+  render: function() {
+    // since JSX is case sensitive, be sure to use 'encType'
+    return (
+      <form onSubmit={this.handleSubmit} encType="multipart/form-data">
+        <input type="file" onChange={this.handleFile} />
+            <input onClick={this.handleFile} type="submit" />
+      </form>
+    );
+  },
+});
+
 var BeerForm = React.createClass({
 
   handleSubmit: function(e){
@@ -76,3 +119,4 @@ render: function() {
 });
 
 React.render(<BeerForm url="/api/beer/"/>, document.getElementById('beerForm'));
+React.render(<FileForm target="/api/beer/"/>, document.getElementById('fileUpload'));
