73efbdef22092127d321ba44f04f92e7a9ef3348
sreahard <suziequz@msn.com>
2015-11-02 10:32:27 -0700

    worked on beer rating system

---
+++ public/scripts/rateForm.js
@@ -0,0 +1,70 @@
+var RateForm = React.createClass({
+
+  handleSubmit: function(e){
+
+      e.preventDefault();
+
+      var aroma = React.findDOMNode(this.refs.aroma).value.trim();
+      var appearance = React.findDOMNode(this.refs.appearance).value.trim();
+      var taste = React.findDOMNode(this.refs.taste).value.trim();
+      var overall = React.findDOMNode(this.refs.overall).value.trim();
+
+      if(!aroma){
+        return;
+      }
+
+      var data = ({aroma: aroma, appearance: appearance, taste: taste, overall: overall, abv: abv, location: location, brewery: brewery, description: description});
+
+          $.ajax({
+              url: this.props.url,
+              dataType: 'json',
+              data: data,
+              type:'POST',
+                  success: function(response){
+                  console.log("posting data!",data, response)
+                  document.location='/'
+                  }.bind(this),
+                  error: function(xhr, status, err){
+                      console.log("not posting data!")
+                      console.error(this.props.url, status, err.toString());
+                  }.bind(this)
+          })
+  }
+  ,
+
+
+render: function() {
+      return (
+
+               <div className="col-sm-6 col-md-8">
+                <div className="row">
+                <h1>Enter New Beers</h1>
+                <hr/>
+                <form>
+                  <div className="form-group">
+                      <label>Beer Name</label>
+                      <input type="text" className="form-control" ref="aroma" placeholder="Beer Name"/>
+                  </div>
+                  <div className="form-group">
+                      <label>appearance URL</label>
+                      <input type="text" className="form-control" ref="appearance" placeholder="appearance URL"/>
+                  </div>
+
+                  <div className="form-group">
+                      <label>Type of Beer </label>
+                      <input type="author" className="form-control" ref="taste" placeholder="Type of Beer"/>
+                  </div>
+                  <div className="form-group">
+                      <label>overall</label>
+                      <input className="form-control" ref="overall" placeholder="overall"/>
+                  </div>
+                  <button onClick={this.handleSubmit} type="submit" className="btn btn-default"> Submit </button>
+              </form>
+               </div>
+               </div>
+
+          );
+  }
+});
+
+React.render(<RateForm url="/api/ratings"/>, document.getElementById('rateForm'));
--- public/scripts/renderBeer.js
+++ public/scripts/renderBeer.js
@@ -15,12 +15,14 @@ var BeerList = React.createClass({
             fltr: null
         })
     },
+    toggleInfo: function (beerInfo) {
+        this.setState({
+            fltr: beerInfo
+        })
+    },
     render: function() {
-        window.cat = this.state.fltr;
-        window.toggle = this.toggle;
         var that = this;
         var beerCats = [];
-
         this.props.data.map(function(beer){
             if(beerCats.indexOf(beer.category) === -1) {
                 beerCats.push(beer.category);
@@ -37,41 +39,58 @@ var BeerList = React.createClass({
                 <button className="beer-cat" onClick={that.toggle.bind(that, category)}>{category}</button>
                 )
         });
+
+        // var imagePlaceHolder = this.props.data.map(function(beer){
+        //     if (beer.image === null) {
+        //         return "http://www.beerglasseshq.com/images/pintglass.gif"
+        //     } else {
+        //         return beer.image
+        //     }
+        // });
+        // var beerInfo = this.props.data.map(function(beerInfo){
+        //     return (
+        //         <button className="btn btn-s btn-default" onClick={that.toggle.bind(that, beerInfo)}>{beerInfo.name}</button>
+        //         )
+        // });
         var beerData = this.props.data.map(function(beer){
-         if (beer.category === this.state.fltr || !this.state.fltr)
-             return (
-                 <div className="col-sm-6 col-md-4">
-                 <div className="row beer-display">
+           if (beer.category === this.state.fltr || !this.state.fltr)
+               return (
+                    <div className="col-sm-6 col-md-4">
+                    <div className="beer-display">
+                    <div className="row">
+                   <div className="well-beer">
+                   <img src={beer.image} className="img-responsive"/>
+                   <div className="caption">
+                   <h3>{beer.name}</h3><br/>
+                   <h4>
+                   {beer.abv > 0 ? ' ABV ' + beer.abv + '% / ': ''} {beer.ibu}{beer.ibu > 0 ? ' IBU / ' : ''}{beer.location}
+                   </h4>
+                   <hr className="short-rule"/>                        
+                   <p className="brewery">
+                   {beer.brewery}
+                   </p>
+                   <p className="rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star-half-o"></i><i className="fa fa-star-o"></i></p>
+                    <a href="/rate"><button type="button" className="btn btn-s btn-default"><i className="fa fa-beer"></i> Rate</button></a> &nbsp;
+                    <button type="button" className="btn btn-s btn-default"><i className="fa fa-info-circle"></i> {beer.name}</button> 
+                    </div>
+                    </div>
+                   </div>
+                   </div>
+                   </div>
+                   )
+}.bind(this));
+return (
 
-                 <div className="thumbnail">
-                 <img src="./images/beer.jpeg" alt="Lorem Pixel"/>
-                 <div className="caption">
-                 <h3>{beer.name}</h3><br/>
-                 <h4>
-                  {beer.abv > 0 ? ' ABV ' + beer.abv + '% / ': ''} {beer.ibu}{beer.ibu > 0 ? ' IBU / ' : ''}{beer.location}
-                 </h4>
-                 <hr className="short-rule"/>                        
-                 <p className="brewery">
-                 {beer.brewery}
-                 </p>
+    <div>
+    <div>
+    <button className="beer-cat" onClick={that.reToggle}>All</button>
+    {beerButtons}
+    </div>                 
+    {beerData}
+    </div>
 
-                 </div>
-                 </div>
-                 </div>
-                 </div>
-                 )
-     }.bind(this));
-        return (
-
-            <div>
-            <div>
-            <button className="beer-cat" onClick={that.reToggle}>All</button>
-            {beerButtons}
-            </div>
-            {beerData}
-            </div>
-            );
-    }
+    );
+}
 });
 
 
@@ -97,18 +116,18 @@ var App = React.createClass({
     });
 },
 
-    componentDidMount: function(){
-        this.loadBeers();
-    },
+componentDidMount: function(){
+    this.loadBeers();
+},
 
 
-    render: function() {
-        return (
-            <div>
-            <BeerList data={this.state.data}/>
-            </div>
-            )
-    }
+render: function() {
+    return (
+        <div>
+        <BeerList data={this.state.data}/>
+        </div>
+        )
+}
 })
 
 React.render(<App url="/api/beer/"/>, document.getElementById("beerPosts") )
