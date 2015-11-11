7ea5142c875fabc020ba92340c0429aabc149101
sreahard <suziequz@msn.com>
2015-11-11 10:37:13 -0700

    updated FB and Twitter redirects to point to the heroku app

--- config/auth.js
+++ config/auth.js
@@ -3,13 +3,13 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '832193756902225', // your App ID
         'clientSecret'  : '4eafaf2af51aa7147575ac6e41493779', // your App Secret
-        'callbackURL'   : 'http://localhost:9090/auth/facebook/callback'
+        'callbackURL'   : 'http://tap-track.herokuapp.com/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'VI4mIGYb3sk6r5GiCwa8m3hrm',
         'consumerSecret'    : 'm3weSnWU7l3nxUCdjmQtEpB10bbUNs3Sv6PJH2Klon4Rpdsmo2',
-        'callbackURL'       : 'http://localhost:9090/auth/twitter/callback'
+        'callbackURL'       : 'http://tap-track.herokuapp.com/auth/twitter/callback'
     }
 
 };
